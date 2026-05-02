4f715a35f768ea8c98e2b8190f3c3713d6335096
Shoaib Santo <44254821+shoaibsanto@users.noreply.github.com>
2026-05-03 00:53:57 +0600

    test

--- smart_qc/settings.py
+++ smart_qc/settings.py
@@ -117,7 +117,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'dashboard'
